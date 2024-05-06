a10f93d5489559bf0b0d6be6228cc3f156bccaa1
jorge.carmona@vertisystem.com <jorge.carmona@vertisystem.com>
2024-05-05 20:28:50 -0400

    feat(theme): added application theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,14 @@
+import {createTheme} from "@mui/material";
+
+const nextHrTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#1570EF"
+    },
+    secondary: {
+      main: "#344054"
+    }
+  }
+});
+
+export default nextHrTheme;
