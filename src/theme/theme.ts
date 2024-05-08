6acb7d7b976b2e4e16a94b98a970642d104872b0
jorge.carmona@vertisystem.com <jorge.carmona@vertisystem.com>
2024-05-07 20:47:35 -0400

    chore(theme): added poppins font and theme for storybook

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,4 +1,5 @@
 import {createTheme} from "@mui/material";
+import {background} from "@storybook/theming";
 
 const nextHrTheme = createTheme({
   palette: {
@@ -7,6 +8,39 @@ const nextHrTheme = createTheme({
     },
     secondary: {
       main: "#344054"
+    }
+  },
+  typography: {
+    fontFamily: "Poppins, sans-serif",
+    fontWeightMedium: 400
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+          "&.tertiary": {
+            backgroundColor: "#F0F9FF",
+            color: "#1849A9",
+            "&:hover": {
+              backgroundColor: "#D0E7FF"
+            }
+          },
+          "&.selected": {
+            color: "#1849A9",
+            fontWeight: 500
+          }
+        },
+        outlined: {
+          backgroundColor: "#FFFFFF",
+          borderColor: "#D0D5DD",
+          color: "#344054",
+          "&:hover": {
+            backgroundColor: "rgba(208, 213, 221, 0.04)",
+            borderColor: "#B0B5BD"
+          }
+        }
+      }
     }
   }
 });
