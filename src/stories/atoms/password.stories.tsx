a000d5b5428c8723a5f205723c3cf6ebd7816469
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-05-14 13:22:35 -0500

    Revert "Revert "feat(stories): Added password component""
    This reverts commit a8d1264fe7df2221bccb10d47059031383bc6b14.

---
+++ src/stories/atoms/password.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import PasswordTextField from "../../atoms/textfield-password";
+
+export default {
+    title: 'Atoms/PasswordTextField',
+    component: PasswordTextField,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'centered',
+    }
+} as Meta <typeof PasswordTextField>;
+
+export const Primary : StoryObj<typeof PasswordTextField> = {
+    args : {
+        label: "",
+        helperText: "",
+        password: "",
+        handlePassword: (event) => {
+        console.log("Password entered:", event.target.value);
+    }
+}
+}
