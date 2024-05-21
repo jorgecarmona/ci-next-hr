dfa7ec0f009eb1f3f2d7f962f27f603889dc5a58
jorge.carmona@vertisystem.com <jorge.carmona@vertisystem.com>
2024-05-21 13:10:55 -0400

    feat: cleaned playground

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -9,30 +9,11 @@ function PlayGround() {
 
   console.log(data);
 
-  const [password, setPassword] = React.useState("");
-
-  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value); 
-};
-  const response = {
-    error: false
-  };
-
   return (
     <>
       <h1>TextField Password</h1>
-      <PasswordTextField 
-      error={response.error}
-      errorHelperText="Usuario o contraseña incorrectas"
-      fullWidth
-      handlePassword={handlePassword}
-      helperText="ingresa tu contraseña"
-      icon
-      label="Password"
-      password={password}
-      />
     </>
-  )
+  );
 }
 
 export default PlayGround;
