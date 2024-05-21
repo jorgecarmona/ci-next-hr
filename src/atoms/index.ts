07e1860cf89f72792eec8ccce7fabc0ef98658f1
Sergio Gutiérrez <136516495+SGutierrez17@users.noreply.github.com>
2024-05-21 12:05:25 -0500

    Add favicon to application (#7)
    * feat(theme): added application theme
    
    * feat(button): added button component
    
    * chore(theme): added poppins font and theme for storybook
    
    * feat(button): added stories
    
    * feat(button): fixed color for default
    
    * feat(icon-store): added visibility,visibilityoff
    
    * feat(password-textfield): added password component
    
    * feat(stories): Added password component
    
    * Revert "feat(stories): Added password component"
    
    This reverts commit 62ba5c181b8f59c85547ac247ab9454efd10ffea.
    
    * Revert "Revert "feat(stories): Added password component""
    
    This reverts commit a8d1264fe7df2221bccb10d47059031383bc6b14.
    
    * chore(password): added storybook
    
    * chore(password): Added unit test
    
    * feat(textfield): created new textfield component
    
    * chore(textfield): added storybook
    
    * chore(textfield): added unit test
    
    * feat(textfield): added email icon
    
    * feat(favicon): added favicon to application
    
    * feat(favicon): added name to application
    
    ---------
    
    Co-authored-by: jorge.carmona@vertisystem.com <jorge.carmona@vertisystem.com>

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,4 +1,7 @@
 import Button from "./button";
 import Typography from "./typography";
+import TextFieldPassword from "./textfield-password";
 
 export {Button, Typography};
+
+export {TextFieldPassword};
