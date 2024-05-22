599b8dda8d39e4cf5680f44fa3c0a185030baf93
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-05-21 21:47:28 -0500

    Revert "feat(textfield):put spaces between each render"
    This reverts commit 089a4b4eb46997c8f83c7a3b76038f0db4d59719.

--- src/__tests__/atoms/textField.test.tsx
+++ src/__tests__/atoms/textField.test.tsx
@@ -3,21 +3,18 @@ import { render, screen } from "@testing-library/react"
 import TextField from "../../atoms/textField"
 
 describe("TextField Component", () => {
-
     it('renders the label correctly', () => {
         render(<TextField label="Name" />);
         const labelElement = screen.getByText(/Name/i);
         expect(labelElement).toBeInTheDocument();
     });
 });
-
     it('displays the required asterisk when required is true', () => {
         render(<TextField label="Email" required />);
         const asteriskElement = screen.getByText('*');
         expect(asteriskElement).toBeInTheDocument();
         expect(asteriskElement).toHaveStyle({ color: 'red' });
 });
-
     it("renders textfield with icon and helpertext",()=>{
         render(
             <TextField
@@ -26,7 +23,6 @@ describe("TextField Component", () => {
             />
         )
     });
-
     it('does not display the icon when icon is false', () => {
         render(<TextField label="Email" icon={false} />);
         const iconElement = screen.queryByRole('button');
