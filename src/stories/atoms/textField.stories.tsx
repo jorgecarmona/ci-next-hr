2b2ff3fdfdba424fce993e18f8786838fdac0f5b
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-05-21 21:56:41 -0500

    chore(textfield): added stories

--- src/stories/atoms/textField.stories.tsx
+++ src/stories/atoms/textField.stories.tsx
@@ -11,13 +11,40 @@ export default {
     }
 } as Meta <typeof TextField>;
 
-export const Primary : StoryObj<typeof TextField> = {
+export const Default : StoryObj<typeof TextField> = {
     args : {
-        label: "",
-        value:"",
-        placeholder:"",
-        helperText:"",
+        label: "Name",
+        value:"Massachusetts Paid Family & Medical Leave",
+        fullWidth: true
     }
 }
+
+export const IconTextField: StoryObj<typeof TextField> = {
+    args: {
+        label: "Email",
+        value: "",
+        icon: true,
+        placeholder:"Enter your email here"
+}
+}
+
+export const ReadOnlyTextField: StoryObj<typeof TextField> = {
+    args: {
+        label: "Code",
+        value: "PF-ML",
+        readOnly: true
+}
+}
+
+export const ErrorsTextField: StoryObj<typeof TextField> = {
+    args: {
+        label: "Name",
+        value: "",
+        error: true,
+        helperText: "This field is required",
+        required: true
+}
+}
+
 
 
