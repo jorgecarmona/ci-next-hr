94f32ee089e11024ee2a677a89e80342770cc21f
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-05-21 21:45:36 -0500

    feat(textfield):setting a gray color for read-only fields and adding a handle change function

--- src/atoms/textField.tsx
+++ src/atoms/textField.tsx
@@ -1,19 +1,22 @@
-import {TextField as MuiTextField, IconButton, InputAdornment, InputLabel } from "@mui/material";
+import {ChangeEvent} from "react";
 
+import {TextField as MuiTextField, IconButton, InputAdornment, InputLabel } from "@mui/material";
 import { iconLookup } from "./icon-store";
 
 interface TextFieldProps{
-    label: string;
-    value?: string;
-    required?: boolean;
-    placeholder?: string;
-    icon?: boolean;
     error?: boolean;
+    fullWidth?: boolean;
     helperText?: string;
-    fullWidth?:boolean;
-    readOnly?:boolean
+    icon?: boolean;
+    label: string;
+    onChangeTextField: (value: string) => void;
+    placeholder?: string;
+    readOnly?: boolean;
+    required?: boolean;
+    value?: string;
 }
-function TextField({fullWidth, label, value, required, placeholder, icon, error, helperText, readOnly}:TextFieldProps){
+
+function TextField({error,fullWidth,helperText,icon,label,onChangeTextField,placeholder,readOnly,required, value}:TextFieldProps){
 
     const renderEmailIcon = () => {
         if (icon) {
@@ -30,26 +33,30 @@ function TextField({fullWidth, label, value, required, placeholder, icon, error,
         return null;
     };
 
+    const handleChange = (e:ChangeEvent<HTMLInputElement>) => onChangeTextField(e.target.value);
+
     return(
         <>
             <InputLabel required={false} style={{paddingLeft:"10px"}}>
                 {label} {required && <span style={{color: "red"}}>*</span>}
             </InputLabel>
             <MuiTextField 
-            style={{paddingLeft:"10px"}}
+            fullWidth={fullWidth}
+            style={{backgroundColor: readOnly ? "#eeeeee" : "transparent"}}
             id="TextField"
             size="small"
-            // label={label}
             value={value}
+            onChange={handleChange}
             placeholder={placeholder}
             error={error}
             helperText={helperText}
             InputProps={{
                 startAdornment: renderEmailIcon(),
-                readOnly: readOnly
+                readOnly: readOnly,
             }}
             />
         </>
     );
 }
 export default TextField
+
