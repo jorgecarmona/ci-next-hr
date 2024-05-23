7d0a3e28ccb350d112339f9f859e9ec1cbed54f7
David Cedillo <137354680+davidc0326@users.noreply.github.com>
2024-05-22 20:45:32 -0400

    fix: resolved merge conflict and integrated changes for MuiChip and MuiCard

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,5 +1,4 @@
-// Theme.tsx
-import {createTheme} from "@mui/material";
+import { createTheme } from "@mui/material";
 
 const nextHrTheme = createTheme({
   palette: {
@@ -65,7 +64,7 @@ const nextHrTheme = createTheme({
         root: {
           fontFamily: "Poppins, sans-serif",
           fontWeight: 500,
-          borderWidth:"1.10px",
+          borderWidth: "1.10px",
           "&.pending": {
             backgroundColor: "#EFF8FF",
             color: "#175CD3"
@@ -101,6 +100,80 @@ const nextHrTheme = createTheme({
         }
       }
     },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          borderWidth: "1px",
+          fontFamily: "Poppins, sans-serif",
+          padding: "16px"
+        }
+      }
+    },
+    MuiCardActions: {
+      styleOverrides: {
+        root: {
+          padding: "0"
+        }
+      }
+    },
+    MuiCardContent: {
+      styleOverrides: {
+        root: {
+          padding: "0"
+        }
+      }
+    },
+    MuiCardHeader: {
+      styleOverrides: {
+        root: {
+          padding: "0",
+          marginBottom: "16px"
+        },
+        title: {
+          color: "#000000",
+          fontSize: "14px",
+          fontWeight: "600",
+          lineHeight: "20px",
+          textAlign: "left"
+        }
+      }
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          fontWeight: 500
+        },
+        body1: {
+          fontWeight: 400,
+          fontSize: "1rem",
+          lineHeight: "1.5rem"
+        },
+        h1: {
+          fontSize: "2.25rem",
+          lineHeight: "2.75rem"
+        },
+        h2: {
+          fontSize: "2rem",
+          lineHeight: "2.5rem"
+        },
+        h3: {
+          fontSize: "1.5rem",
+          lineHeight: "2.25rem"
+        },
+        h4: {
+          fontSize: "1.25rem",
+          lineHeight: "1.875rem"
+        },
+        h5: {
+          fontSize: "1.125rem",
+          lineHeight: "1.75rem"
+        },
+        h6: {
+          fontSize: "1rem",
+          lineHeight: "1.5rem"
+        }
+      }
+    }
   }
 });
 
