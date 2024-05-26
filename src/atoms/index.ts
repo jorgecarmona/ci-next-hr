bce1e17fd4752ec2cdba8494c674765c90c74145
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-05-26 12:17:30 -0500

    import UnderlineLink component into the index.ts

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -2,5 +2,6 @@ import Button from "./button";
 import Card from "./card";
 import Typography from "./typography";
 import PasswordTextField from "./textfield-password";
+import UnderlineLink from "./link";
 
-export {Button, Card, Typography, PasswordTextField};
+export {Button, Card, PasswordTextField, Typography, UnderlineLink};
