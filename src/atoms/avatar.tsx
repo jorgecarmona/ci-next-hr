17c047c52552019a940ac5fda517b4e8b1613b5c
Ingrid Julieth <injuvillalba10@gmail.com>
2024-05-26 20:42:12 -0500

    feat(avatar): create avatar component

--- src/atoms/avatar.tsx
+++ src/atoms/avatar.tsx
@@ -6,8 +6,8 @@ interface AvatarProps extends MuiAvatarProps {
   alt?: string;
   children?: React.ReactNode;
   height?: number;
-  src?: string;
-  type?: "profile" | "image" | undefined;
+  // src?: string;
+  type?: "profile";
   width?: number;
 }
 
@@ -17,10 +17,44 @@ function Avatar({alt, children, height, src, type, width}: AvatarProps) {
   }
 
   return (
-    <MuiAvatar src={src} sx={{width, height}}>
+    <MuiAvatar alt={alt} src={src} sx={{width, height}} style={{background: type?"#072136":undefined}}>
       {type === "profile" ? children : null}
     </MuiAvatar>
   );
 }
 
 export default Avatar;
+
+
+// import React from 'react';
+// import {Avatar as MuiAvatar, AvatarProps as MuiAvatarProps} from "@mui/material";
+// import theme from "../theme/theme"
+
+// interface AvatarProps extends MuiAvatarProps {
+//   alt?: string;
+//   children?: React.ReactNode;
+//   className?: string;
+//   height: number;
+//   src?: string;
+//   type?: 'profile';
+//   width: number;
+// };
+
+// function Avatar({ alt, children, className, height, src, type, width}:AvatarProps){
+
+//   if (type === 'profile') {
+//     return (
+//       <MuiAvatar className={className} height={height} width={width} >
+//         {children}
+//       </MuiAvatar>
+//     );
+//   }
+
+//   return (
+//     <MuiAvatar alt={alt} className={className} height={height} src={src} width={width} >
+//       {children}
+//     </MuiAvatar>
+//   );
+// };
+
+// export default Avatar;
