a62fb3b91034c47fac664d6455a4e92685cf223e
Ingrid Julieth <injuvillalba10@gmail.com>
2024-05-27 17:52:03 -0500

    feat(avatar): changes in the component

--- src/atoms/avatar.tsx
+++ src/atoms/avatar.tsx
@@ -6,7 +6,7 @@ interface AvatarProps extends MuiAvatarProps {
   alt?: string;
   children?: React.ReactNode;
   height?: number;
-  // src?: string;
+  src?: string;
   type?: "profile";
   width?: number;
 }
@@ -16,45 +16,18 @@ function Avatar({alt, children, height, src, type, width}: AvatarProps) {
     console.error("The 'children' property is required when type is 'profile'.");
   }
 
+  const sxProps = type === "profile" ? {} : { width, height };
+
   return (
-    <MuiAvatar alt={alt} src={src} sx={{width, height}} style={{background: type?"#072136":undefined}}>
+    <MuiAvatar
+      alt={alt}
+      src={src}
+      sx={sxProps}
+      style={{ background: type ? "#072136" : undefined }}
+    >
       {type === "profile" ? children : null}
     </MuiAvatar>
   );
 }
 
 export default Avatar;
-
-
-// import React from 'react';
-// import {Avatar as MuiAvatar, AvatarProps as MuiAvatarProps} from "@mui/material";
-// import theme from "../theme/theme"
-
-// interface AvatarProps extends MuiAvatarProps {
-//   alt?: string;
-//   children?: React.ReactNode;
-//   className?: string;
-//   height: number;
-//   src?: string;
-//   type?: 'profile';
-//   width: number;
-// };
-
-// function Avatar({ alt, children, className, height, src, type, width}:AvatarProps){
-
-//   if (type === 'profile') {
-//     return (
-//       <MuiAvatar className={className} height={height} width={width} >
-//         {children}
-//       </MuiAvatar>
-//     );
-//   }
-
-//   return (
-//     <MuiAvatar alt={alt} className={className} height={height} src={src} width={width} >
-//       {children}
-//     </MuiAvatar>
-//   );
-// };
-
-// export default Avatar;
