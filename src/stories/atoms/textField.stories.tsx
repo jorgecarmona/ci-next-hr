0205c41394d25763a99a48d8ae15229f44e37fb7
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-05-26 23:38:17 -0500

    fix(textfield): corrections on storybook

--- src/stories/atoms/textField.stories.tsx
+++ src/stories/atoms/textField.stories.tsx
@@ -2,47 +2,63 @@ import { Meta, StoryObj } from "@storybook/react";
 
 import TextField from "../../atoms/textField";
 
-export default {
+const meta = {
     title: 'Atoms/TextField',
     component: TextField,
     tags: ['autodocs'],
     parameters: {
         layout: 'centered',
     }
-} as Meta <typeof TextField>;
+} satisfies Meta <typeof TextField>; 
 
-export const Default : StoryObj<typeof TextField> = {
+export default meta;
+
+type Story =  StoryObj<typeof meta>;
+
+export const Default : Story = {
     args : {
         label: "Name",
         value:"Massachusetts Paid Family & Medical Leave",
-        fullWidth: true
+        fullWidth: true,
+        onChangeTextField(value) {
+            console.log("TextField entered:", value);
+        },
     }
 }
 
-export const IconTextField: StoryObj<typeof TextField> = {
+export const IconTextField: Story = {
     args: {
         label: "Email",
         value: "",
         icon: true,
-        placeholder:"Enter your email here"
+        placeholder:"Enter your email here",
+        onChangeTextField(value) {
+            console.log("TextField entered:", value);
+        },
 }
 }
 
-export const ReadOnlyTextField: StoryObj<typeof TextField> = {
+export const ReadOnlyTextField: Story = {
     args: {
         label: "Code",
         value: "PF-ML",
-        readOnly: true
+        readOnly: true,
+        onChangeTextField(value) {
+            console.log("TextField entered:", value);
+        },
 }
 }
 
-export const ErrorsTextField: StoryObj<typeof TextField> = {
+export const ErrorsTextField: Story = {
     args: {
         label: "Name",
         value: "",
         error: true,
         helperText: "This field is required",
-        required: true
+        required: true,
+        onChangeTextField(value) {
+            console.log("TextField entered:", value);
+        },
 }
 }
 
