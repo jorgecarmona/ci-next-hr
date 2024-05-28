9dabfe1e0f0518532873eae3caac3d89985bc7c0
Andres Rondone <andresfeliperondon32@gmail.com>
2024-05-27 20:38:23 -0500

    feat: se anadio mui grid props

--- src/atoms/grid.tsx
+++ src/atoms/grid.tsx
@@ -1,17 +1,17 @@
-import React, { ReactNode } from 'react';
-import { Grid as MuiGrid } from '@mui/material';
+import React from 'react';
+import { Grid as MuiGrid, Grid2Props as MuiGridProps } from '@mui/material';
 
-interface GridProps {
+interface GridProps extends MuiGridProps {
   container?: boolean;
-  children?: ReactNode;
+  children?: React.ReactNode;
   item?: boolean;
-  xs?: number | 'auto' | boolean;
 }
 
-function Grid({ container, children, item, xs }: GridProps) {
+function Grid({ container, children, item }: GridProps) {
+
   return (
-    <MuiGrid container={container} item={item} xs={xs} >
-      {children}
+    <MuiGrid container={container} item={item} >
+        {children}
     </MuiGrid>
   );
 };
