1075d1dcdd30dd543b264da3568c4efd6793a9bd
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-05-27 22:22:32 -0500

    feat: fixed index.ts component

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,12 +1,12 @@
 import Autocomplete from "./autocomplete";
 import Avatar from "./avatar";
 import Button from "./button";
-import Grid from "./grid";
 import Card from "./card";
+import Grid from "./grid";
+import Link from "./link";
 import PasswordTextField from "./password-textfield";
 import TextField from "./textField";
 import ToolbarButton from "./toolbar-button";
 import Typography from "./typography";
-import Link from "./link";
 
-export {Avatar, Autocomplete, Button, Card, Grid, Link, PasswordTextField, TextField, ToolbarButton, Typography};
+export {Autocomplete, Avatar, Button, Card, Grid, Link, PasswordTextField, TextField, ToolbarButton, Typography};
