86ce940696ca0e1286f8e48489b216f34006018c
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-05-27 23:19:30 -0500

    feat(index): added import chip component

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -2,6 +2,7 @@ import Autocomplete from "./autocomplete";
 import Avatar from "./avatar";
 import Button from "./button";
 import Card from "./card";
+import Chip from "./chip";
 import Grid from "./grid";
 import Link from "./link";
 import PasswordTextField from "./password-textfield";
@@ -9,4 +10,4 @@ import TextField from "./textField";
 import ToolbarButton from "./toolbar-button";
 import Typography from "./typography";
 
-export {Autocomplete, Avatar, Button, Card, Grid, Link, PasswordTextField, TextField, ToolbarButton, Typography};
+export {Autocomplete, Avatar, Button, Card, Chip, Grid, Link, PasswordTextField, TextField, ToolbarButton, Typography};
