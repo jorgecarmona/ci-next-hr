8ffdc90721c4449f43cb57e644015e8930d61587
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-05-28 20:11:52 -0500

    chore(link): changes stories

--- src/stories/atoms/link.stories.tsx
+++ src/stories/atoms/link.stories.tsx
@@ -14,7 +14,6 @@ export default {
 export const Primary : StoryObj<typeof Link> = {
     args : {
         children: 'Forgot Password?',
-        linkType: 'primary',
         underline: 'always'
     }
 }
@@ -22,7 +21,6 @@ export const Primary : StoryObj<typeof Link> = {
 export const Secondary : StoryObj<typeof Link> = {
     args : {
         children: 'Sign up',
-        linkType: 'secondary',
         underline: 'hover',
     }
 }
@@ -30,7 +28,6 @@ export const Secondary : StoryObj<typeof Link> = {
 export const Default : StoryObj<typeof Link> = {
     args : {
         children: 'Terms & Conditions',
-        linkType: 'primary',
         underline: 'none'
     }
 }
