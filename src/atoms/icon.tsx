09b3d954fd66bdbf793034d8fddede621af17c57
OskarMantilla21 <oskarmantilla1708@gmail.com>
2024-05-29 18:19:31 -0500

    [WIP] refactor-icon-component

--- src/atoms/icon.tsx
+++ src/atoms/icon.tsx
@@ -1,49 +1,52 @@
-import React from "react";
-
-import {Badge, Icon as MuiIcon, IconProps as MuiIconProps} from "@mui/material";
-
-import {iconLookup} from "./icon-store";
+import { Badge, Icon as MuiIcon, IconProps as MuiIconProps} from "@mui/material";
+import { iconLookup } from "./icon-store";
 
 interface IconProps extends MuiIconProps {
-  name: "article" | "business" | "delete" | "help" | "live" | "library" | "notifications" | "work";
+  bgColor?: string;
+  customColor?: string; 
   hasNotifications?: boolean;
+  name: "article" | "business" | "delete" | "help" | "live" | "library" | "notifications" | "work";
   shape?: "circle" | "square";
 }
 
-function Icon({color, name, hasNotifications, shape, ...props}: IconProps) {
+function Icon({ bgColor, customColor = '#344054', hasNotifications, name, shape, ...props}: IconProps){
   const effectiveShape = name === "delete" ? "square" : shape;
-
   const iconProps: MuiIconProps = {
     ...props,
     className: `${props.className || ""} ${name}`,
     style: {
       ...(props.style || {}),
-      ...(effectiveShape === "circle" ? {borderRadius: "50%"} : {})
+      ...(effectiveShape === "circle" ? {borderRadius: "50%"} : {}),
+      ...(effectiveShape === "square" ? {borderRadius: "20%"} : {}),
+      backgroundColor: bgColor, 
+      color: customColor
     }
   };
-
   const IconComponent = iconLookup[name];
 
   if (!IconComponent) {
+    console.warn(`Icon with name "${name}" does not exist in iconLookup`);
     return null;
   }
-
   if (hasNotifications) {
     return (
-      <>
-        <MuiIcon>
-          <Badge variant="dot" color="error" overlap="circular">
-            <IconComponent />
-          </Badge>
-        </MuiIcon>
-      </>
+      <MuiIcon {...iconProps}>
+        <Badge variant="dot" color="error" overlap="circular" anchorOrigin={{vertical:"top", horizontal:"right"}}>
+          <IconComponent />
+        </Badge>
+      </MuiIcon>
     );
   }
-
+  
   return (
-    <MuiIcon {...iconProps} data-testid="name">
-      <IconComponent />
-    </MuiIcon>
+      <MuiIcon {...iconProps} data-testid="name" 
+        sx={{display: "inline-flex", 
+        alignItems: "center", 
+        justifyContent: "center", 
+        width: "48px", 
+        height: "48px",}}>
+        <IconComponent />
+      </MuiIcon>
   );
 }
 
