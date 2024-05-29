86b9cd82fc89faf7dad0782c3ac2f66a2b63db32
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-05-28 20:12:42 -0500

    chore(link): changes unit test

--- src/__tests__/atoms/link.test.tsx
+++ src/__tests__/atoms/link.test.tsx
@@ -3,19 +3,9 @@ import {render, screen} from "@testing-library/react";
 import Link from "../../atoms/link";
 
 describe("Link Component Unit Test", () => {
-    it("should render the Link with correct children", () => {
-        render(
-            <Link linkType="primary" underline="always">
-                Forgot Password?
-            </Link>
-    );
-    const linkElement = screen.getByText('Forgot Password?');
-    expect(linkElement).toBeInTheDocument();
-});
-
     it("should render the Link with primary style and no underline", () => {
         render(
-            <Link linkType="primary" underline="none">
+            <Link>
                 Terms & Conditions
             </Link>
     );
@@ -25,13 +15,11 @@ describe("Link Component Unit Test", () => {
 
     it("should render the secondary Link with additional text", () => {
         render(
-            <Link linkType="secondary" underline="hover">
+            <Link underline="hover">
                 Sign up
             </Link>
     );
-    const spanElement = screen.getByText("Don't have an account?");
     const linkElement = screen.getByText('Sign up');
-    expect(spanElement).toBeInTheDocument();
     expect(linkElement).toBeInTheDocument();
     });
 });
