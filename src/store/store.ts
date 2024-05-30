fc661d30fe790dc2e0765d31885e6f92b9f58b95
Nancy <nancy.blanco.suarez@gmail.com>
2024-05-30 14:54:28 -0500

    chore: added authApi in src/store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,13 +2,15 @@ import {configureStore} from "@reduxjs/toolkit";
 
 // Import API slices
 import {usersApi} from "./api-slice/users-slice";
+import { authApi } from "./api-slice/auth-slice";
 
 export const store = configureStore({
   reducer: {
     // add API slice reducers
-    [usersApi.reducerPath]: usersApi.reducer
+    [usersApi.reducerPath]: usersApi.reducer,
+    [authApi.reducerPath]: authApi.reducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(usersApi.middleware)
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(usersApi.middleware, authApi.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
