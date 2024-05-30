f38f16049794ef6058f1d077a95ed6fe980992b0
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-05-30 14:15:25 -0500

    chore(navitem): added storybook

---
+++ src/stories/molecules/navItem.stories.tsx
@@ -0,0 +1,53 @@
+import { Meta, StoryObj } from "@storybook/react";
+import NavItem from "../../molecules/navItem";
+import { IconType } from "../../atoms/icon-store";
+
+const meta = {
+    title: 'Atoms/NavItem',
+    component: NavItem,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'centered',
+    },
+    argTypes: {
+        icon: {
+            control: "select",
+            options: ["no icon", ...Object.values(IconType)], 
+            }
+    },
+} satisfies Meta<typeof NavItem>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>
+
+export const Reports : Story = {
+    args : {
+        icon: IconType.TrendingUp,
+        title: "REPORTS",
+        onClickCallBack: (value) => {
+            console.log('Click button:', value);
+        },
+    }
+}
+
+export const Account : Story = {
+    args : {
+        icon: IconType.Person2,
+        title: "ACCOUNT",
+        onClickCallBack: (value) => {
+            console.log('Click button:', value);
+        },
+    }
+}
+
+export const Admin : Story = {
+    args : {
+        icon: IconType.Settings,
+        title: "ADMIN",
+        onClickCallBack: (value) => {
+            console.log('Click button:', value);
+        },
+        selected: true
+    }
+}
